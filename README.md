f69c3adcbb0fc7c0e936da1827030a2042394d06
Liam Keegan <liam@keegan.ch>
2021-07-15 14:15:41 +0200

    add script to install kilosort2.5

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 # mease-hpc-setup
 
-work-in-progress setup guide & scripts for running https://github.com/lkeegan/mease-lab-to-nwb on HPC
+work-in-progress setup guide & installation scripts for running https://github.com/lkeegan/mease-lab-to-nwb on HPC
 
-## SDS
+## MLS&WISO Cluster
+
+
 
 ## SDS
 
 Once logged into the cluster, to get SDS access type
-
 ```
 kinit
 ```
 using your SDS@hd service password
 
 The files are located at
-````
+```
 /mnt/sds-hd/sd19b001
 ```
---
+++ setup_kilosort2_5.sh
@@ -0,0 +1,21 @@
+# go to measelab workspace
+cd $(ws_find measelab)
+
+# make matlab available
+module load math/matlab/R2021a
+
+# remove any existing installation
+rm -rf Kilosort-2.5
+
+# download kilosort 2.5
+wget https://github.com/MouseLand/Kilosort/archive/refs/tags/v2.5.tar.gz
+tar xf v2.5.tar.gz
+rm v2.5.tar.gz
+
+# compile kilosort 2.5 GPU matlab code
+cd Kilosort-2.5/CUDA
+matlab -nodesktop -nosplash -r "mexGPUall;quit"
+cd ../../
+
+# tell spikeinterface where it's installed
+export KILOSORT2_5_PATH=$(pwd)/Kilosort-2.5
