04695a73af671f452c580fb0c33687656c07f467
Liam Keegan <liam@keegan.ch>
2021-07-16 14:01:07 +0200

    add spyking-circus

---
+++ scripts/setup_spyking_circus.sh
@@ -0,0 +1,16 @@
+cd $1
+
+# init conda
+source miniconda3/etc/profile.d/conda.sh
+
+# activate env
+conda activate measelab
+
+# make mpi available (openmpi < 3 required apparently?)
+# module load mpi/openmpi/2.1-intel-17.0
+
+# install
+conda install -c spyking-circus spyking-circus -y
+
+# note: apparently installs files to $HOME? should check
+# also see if MPI is working, docs say it needs a hosts file but seem aimed at desktop user?
