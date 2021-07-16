1cb8c70093d43a1474f7ac692ade20d2df5c3e6e
Liam Keegan <liam@keegan.ch>
2021-07-16 15:31:11 +0200

    add mountainsort4

--- scripts/setup_all.sh
+++ scripts/setup_all.sh
@@ -1,8 +1,20 @@
 INSTALL=/gpfs/bwfor/work/ws/hd_uk239-measelab
 
+# miniconda
+
 bash setup_conda.sh $INSTALL
 
+# mease-lab-to-nwb & measelab env
+
 bash setup_measelab.sh $INSTALL
+
+# sorters
+
+bash setup_hdsort.sh $INSTALL
+
+bash setup_herdingspikes2.sh $INSTALL
+
+bash setup_ironclust.sh $INSTALL
 
 bash setup_kilosort.sh $INSTALL
 
@@ -12,8 +24,12 @@ bash setup_kilosort2_5.sh $INSTALL
 
 bash setup_kilosort3.sh $INSTALL
 
+bash setup_klusta.sh $INSTALL
+
+bash setup_mountainsort4.sh $INSTALL
+
 bash setup_spyking_circus.sh $INSTALL
 
-bash setup_herdingspikes2.sh $INSTALL
+# init script for users
 
 cp init.sh $INSTALL/init.sh
