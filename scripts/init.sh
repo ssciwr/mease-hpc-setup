2c51d90b31205b1101ad0ca672d2ca03b4beeccd
Liam Keegan <liam@keegan.ch>
2021-07-16 15:16:59 +0200

    update ironclust: NOTE manual install steps currently required - see notes in script

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,13 +3,16 @@
 # make matlab available
 module load math/matlab/R2021a
 
+# make cuda available
+module load devel/cuda/11.2
+
 # tell spikeinterface where sorters are installed
 export KILOSORT_PATH=/gpfs/bwfor/work/ws/hd_uk239-measelab/Kilosort-1.0
 export KILOSORT2_PATH=/gpfs/bwfor/work/ws/hd_uk239-measelab/Kilosort-2.0
 export KILOSORT2_5_PATH=/gpfs/bwfor/work/ws/hd_uk239-measelab/Kilosort-2.5
 export KILOSORT3_PATH=/gpfs/bwfor/work/ws/hd_uk239-measelab/Kilosort
 export HDSORT_PATH=/gpfs/bwfor/work/ws/hd_uk239-measelab/HDsort
-
+export IRONCLUST_PATH=/gpfs/bwfor/work/ws/hd_uk239-measelab/ironclust
 # add conda to path
 source /gpfs/bwfor/work/ws/hd_uk239-measelab/miniconda3/etc/profile.d/conda.sh
 
--- scripts/setup_ironclust.sh
+++ scripts/setup_ironclust.sh
@@ -3,6 +3,9 @@ cd $1
 # make matlab available
 module load math/matlab/R2021a
 
+# make cuda available
+module load devel/cuda/11.2
+
 # workaround for git ssl isse
 export GIT_SSL_NO_VERIFY=true
 
@@ -10,5 +13,17 @@ export GIT_SSL_NO_VERIFY=true
 git clone https://github.com/flatironinstitute/ironclust
 
 # compile ironclust GPU matlab code
+
+# NOTE: this needs to run on a node with the GPU!
+
+# NOTE: Fails to autodetect the path to nvcc (and doesn't just try nvcc)
+# need to set vcPath_nvcc in matlab/default.cfg
+# For now have just hard-coded this BY HAND on the cluster:
+# /gpfs/bwfor/lhome/software/common/devel/cuda/11.2.2/bin/nvcc
+
+# have done the above & then ran the below by hand using RTX2080 node
+# should check if this works on other nodes
+# and at some point this should happen as part of the script
+
 cd ironclust/matlab
 matlab -nodesktop -nosplash -r "irc2 compile;quit"
