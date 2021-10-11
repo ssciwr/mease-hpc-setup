dd298ea6eac30117bc87eac0471235f5d51d42c9
Liam Keegan <liam@keegan.ch>
2021-10-11 14:52:49 +0200

    use srun to get token from compute node directly instead of via output file

--- setup_jupyter/setup_jupyter/__init__.py
+++ setup_jupyter/setup_jupyter/__init__.py
@@ -5,16 +5,7 @@ import time
 import os
 
 
-def jupyter_lab_count():
-    return len(json.loads(subprocess.getoutput("jupyter lab list --jsonlist")))
-
-
-def get_jupyter_lab_token():
-    return json.loads(subprocess.getoutput("jupyter lab list --jsonlist"))[0]["token"]
-
-
 def start_jupyter_lab(ip, port):
-    assert jupyter_lab_count() == 0, "Error: Jupyter Lab server already running."
     print("1. Starting Jupyter Lab server...", end="")
     p = subprocess.Popen(
         ["jupyter", "lab", "--no-browser", f"--port={port}", f"--ip={ip}"],
@@ -29,52 +20,56 @@ def start_jupyter_lab(ip, port):
     return p
 
 
+port = 54736
+
+
+def start():
+    assert (
+        hostname[:5] != "login"
+    ), "Error: This is a login node, setup-jupyter should be ran on a compute node."
+    ip = subprocess.getoutput('getent hosts $(uname -n) | head -1 | cut -d " " -f 1')
+    subproc = start_jupyter_lab(ip, port)
+    subproc.wait()
+
+
 def print_instructions(port, hostname, userid, token):
     print("2. Run this command in a new terminal on *your* computer:")
     print(
         f"\nssh -L{port}:{hostname}:{port} {userid}@bwforcluster.bwservices.uni-heidelberg.de\n"
     )
     print("3. Open this address in a web browser to access the jupyter notebook:\n")
-    print(f"localhost:54736/?token={token}\n")
+    print(f"localhost:54736/?token={token}\n", flush=True)
 
 
-def start():
-    port = 54736
-    hostname = subprocess.getoutput("hostname")
-    assert (
-        hostname[:5] != "login"
-    ), "Error: This is a login node, setup-jupyter should be ran on a compute node."
-    ip = subprocess.getoutput('getent hosts $(uname -n) | head -1 | cut -d " " -f 1')
-    userid = subprocess.getoutput("whoami")
-    subproc = start_jupyter_lab(ip, port)
-    token = get_jupyter_lab_token()
-    print_instructions(port, hostname, userid, token)
-    subproc.wait()
+def get_jupyter_lab_token(job_id):
+    return json.loads(
+        subprocess.getoutput(f"srun --jobid={job_id} jupyter lab list --jsonlist")
+    )[0]["token"]
 
 
-status_filename = ".setup-jupyter-status.txt"
-
-
-def clear_status_file():
-    try:
-        os.remove(status_filename)
-    except OSError:
-        pass
+def get_scontrol_output(jobid):
+    data = {}
+    scontrol_output = subprocess.getoutput(f"scontrol show job {jobid}")
+    for item in scontrol_output.split:
+        key, value = item.split("=")
+        data[key] = value
 
 
 def submit():
-    clear_status_file()
     minutes = 30
     gpus = 0
-    sbatch_cmd = (
-        f"sbatch --parsable -t{minutes} --output={status_filename} setup-jupyter-start"
-    )
+    sbatch_cmd = f"sbatch --parsable -t{minutes} --output=.setup-jupyter-log.txt setup-jupyter-start"
     print(sbatch_cmd)
     job_id = subprocess.getoutput(sbatch_cmd)
     print(f"Submitted job with id {job_id}...", end="", flush=True)
     time.sleep(2)
-    while not os.path.exists(status_filename):
+    output = get_scontrol_output(job_id)
+    print(output)
+    while output["JobState"] != "RUNNING":
         print(".", end="", flush=True)
         time.sleep(2)
-    with open(status_filename) as f:
-        print(f.read())
+        output = get_scontrol_output(job_id)
+        print(output)
+    userid = subprocess.getoutput("whoami")
+    token = get_jupyter_lab_token(job_id)
+    print_instructions(port, scontrol_output["BatchHost"], userid, token)
