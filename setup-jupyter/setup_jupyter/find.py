b199deb114fe15eeb3839b586b028ca3294177e8
Liam Keegan <liam@keegan.ch>
2021-12-17 09:39:18 +0100

    update cuda module version, fix typo in jupyter find

--- setup-jupyter/setup_jupyter/find.py
+++ setup-jupyter/setup_jupyter/find.py
@@ -7,5 +7,5 @@ import click
 @click.argument("job_id", required=False, default=-1)
 def find(job_id):
     if job_id == -1:
-        job_id = subprocess.getoutput("squeue - t RUNNING - o % i - h | head -n 1")
+        job_id = subprocess.getoutput("squeue -t RUNNING -o %i -h | head -n 1")
     util.wait_for_info_and_print(job_id)
